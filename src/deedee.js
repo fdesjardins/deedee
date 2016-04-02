e89b192b0d23345f12e83d7b6a8aec5f5d20acb9
Forrest Desjardins <desjardinsfg@gmail.com>
2016-04-01 22:49:42 -0800

    Refactor

--- src/deedee.js
+++ src/deedee.js
@@ -4,45 +4,86 @@ import path from 'path';
 import chalk from 'chalk';
 import table from 'text-table';
 import walk from 'walk';
+import _ from 'lodash';
 
-function detectNodeDependencies(json) {
-	var out = [];
-	for (const dep in json.devDependencies) {
-		if (json.devDependencies.hasOwnProperty(dep)) {
-			out.push([chalk.blue(dep), chalk.blue(json.devDependencies[dep])]);
+const colorMap = {
+	node: {
+		dependencies: {
+			title: chalk.underline.blue,
+			item: chalk.blue
+		},
+		devDependencies: {
+			title: chalk.underline.green,
+			item: chalk.green
+		}
+	},
+	bower: {
+		dependencies: {
+			title: chalk.underline.red,
+			item: chalk.red
+		},
+		devDependencies: {
+			title: chalk.underline.magenta,
+			item: chalk.magenta
 		}
 	}
-	console.log(table(out));
+};
+
+function print(deps) {
+	var dependencies = deps.dependencies;
+	var devDependencies = deps.devDependencies;
+
+	if (deps.type === 'node') {
+		if (deps.dependencies.length > 0) {
+			const titleColor = colorMap.node.dependencies.title;
+			console.log(titleColor(`${deps.projectName} - package.json (dependencies)`));
+			console.log(table(_.map(dependencies, dep => {
+				const itemColor = colorMap.node.dependencies.item;
+				return [itemColor(dep.name), itemColor(dep.version)];
+			})));
+			console.log(os.EOL);
+		}
+
+		if (deps.devDependencies.length > 0) {
+			const titleColor = colorMap.node.devDependencies.title;
+			console.log(titleColor(`${deps.projectName} - package.json (devDependencies)`));
+			console.log(table(_.map(devDependencies, dep => {
+				const itemColor = colorMap.node.devDependencies.item;
+				return [itemColor(dep.name), itemColor(dep.version)];
+			})));
+			console.log(os.EOL);
+		}
+	}
+	else if (deps.type === 'bower') {
+		if (deps.dependencies.length > 0) {
+			const titleColor = colorMap.bower.dependencies.title;
+			console.log(titleColor(`${deps.projectName} - bower.json (dependencies)`));
+			console.log(table(_.map(dependencies, dep => {
+				const itemColor = colorMap.bower.dependencies.item;
+				return [itemColor(dep.name), itemColor(dep.version)];
+			})));
+			console.log(os.EOL);
+		}
+
+		if (deps.devDependencies.length > 0) {
+			const titleColor = colorMap.bower.devDependencies.title;
+			console.log(titleColor(`${deps.projectName} - bower.json (devDependencies)`));
+			console.log(table(_.map(devDependencies, dep => {
+				const itemColor = colorMap.bower.devDependencies.item;
+				return [itemColor(dep.name), itemColor(dep.version)];
+			})));
+			console.log(os.EOL);
+		}
+	}
 }
 
-function detectNodeDevDependencies(json) {
-	var out = [];
-	for (const dep in json.dependencies) {
-		if (json.dependencies.hasOwnProperty(dep)) {
-			out.push([chalk.green(dep), chalk.green(json.dependencies[dep])]);
-		}
-	}
-	console.log(table(out));
-}
-
-function detectBowerDependencies(json) {
-	var out = [];
-	for (const dep in json.dependencies) {
-		if (json.dependencies.hasOwnProperty(dep)) {
-			out.push([chalk.red(dep), chalk.red(json.dependencies[dep])]);
-		}
-	}
-	console.log(table(out));
-}
-
-function detectBowerDevDependencies(json) {
-	var out = [];
-	for (const dep in json.devDependencies) {
-		if (json.dependencies.hasOwnProperty(dep)) {
-			out.push([chalk.magenta(dep), chalk.magenta(json.dependencies[dep])]);
-		}
-	}
-	console.log(table(out));
+function extractNodeOrBower(json, fieldName) {
+	return _.map(json[fieldName], (version, name) => {
+		return {
+			name: name,
+			version: version
+		};
+	});
 }
 
 function detectNode(root) {
@@ -50,14 +91,12 @@ function detectNode(root) {
 	let packageJson = JSON.parse(fs.readFileSync(configPath));
 	let projectName = packageJson.name;
 
-	console.log(os.EOL);
-
-	console.log(chalk.bgBlue(`${projectName} - package.json (dependencies)`));
-	detectNodeDependencies(packageJson);
-	console.log(os.EOL);
-
-	console.log(chalk.bgGreen(`${projectName} - package.json (devDependencies)`));
-	detectNodeDevDependencies(packageJson);
+	return {
+		type: 'node',
+		projectName: projectName,
+		dependencies: extractNodeOrBower(packageJson, 'dependencies'),
+		devDependencies: extractNodeOrBower(packageJson, 'devDependencies')
+	};
 }
 
 function detectBower (root, filename) {
@@ -65,17 +104,17 @@ function detectBower (root, filename) {
 	let bowerJson = JSON.parse(fs.readFileSync(configPath));
 	let projectName = bowerJson.name;
 
-	console.log(os.EOL);
-
-	console.log(chalk.bgRed(`${projectName} - bower.json (dependencies)`));
-	detectBowerDependencies(bowerJson);
-	console.log(os.EOL);
-
-	console.log(chalk.bgMagenta(`${projectName} - bower.json (devDependencies)`));
-	detectBowerDevDependencies(bowerJson);
+	return {
+		type: 'bower',
+		projectName: projectName,
+		dependencies: extractNodeOrBower(bowerJson, 'dependencies'),
+		devDependencies: extractNodeOrBower(bowerJson, 'devDependencies')
+	};
 }
 
 export default function (options) {
+
+	console.log(os.EOL);
 
 	if (options.recursive) {
 
@@ -86,11 +125,11 @@ export default function (options) {
 
 		walker.on('file', (root, fstat, next) => {
 			if (fstat.name == 'package.json') {
-				detectNode(root);
+				print(detectNode(root));
 			}
 
 			if (fstat.name == 'bower.json') {
-				detectBower(root);
+				print(detectBower(root));
 			}
 			next();
 		});
@@ -101,13 +140,11 @@ export default function (options) {
 		const bowerJsonPath = `${options.path}/bower.json`;
 
 		if (fs.existsSync(packageJsonPath)) {
-			detectNode(options.path);
+			print(detectNode(options.path));
 		}
 
 		if (fs.existsSync(bowerJsonPath)) {
-			detectBower(options.path);
+			print(detectBower(options.path));
 		}
 	}
-
-	console.log(os.EOL);
 }
