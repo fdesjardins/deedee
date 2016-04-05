95bbf0df6f5fcba306ff232473ebd433ea2068ff
Forrest Desjardins <desjardinsfg@gmail.com>
2016-04-04 22:56:18 -0800

    Add main lib bower.json tests

--- test/deedee.js
+++ test/deedee.js
@@ -8,19 +8,21 @@ import * as bowerJson from './fixture/bower.json';
 
 import { __RewireAPI__ as deedeeRewireAPI } from '../lib/deedee.js';
 
+chai.should();
+
 describe('deedee', () => {
 
 	const extractNodeOrBower = deedeeRewireAPI.__get__('extractNodeOrBower');
 
 	describe('package.json', () => {
-		let deps = [
+		const deps = [
 			{ name: 'assert', version: '^1.3.0' },
 			{ name: 'glob', version: '^5.0.15' },
 			{ name: 'has', version: '^1.0.0' },
 			{ name: 'lodash', version: '^4.6.1' }
 		];
 
-		let devDeps = [
+		const devDeps = [
 			{ name: 'babel-cli', version: '^6.6.5' },
 		  { name: 'babel-core', version: '^6.7.2' },
 		  { name: 'chai', version: '^3.5.0' }
@@ -44,6 +46,41 @@ describe('deedee', () => {
 		  		projectName: 'test',
 				  dependencies: deps,
 				  devDependencies: devDeps
+				});
+		});
+	});
+
+	describe('bower.json', () => {
+		const deps = [
+			{ name: 'bootstrap', version: '^3.3.6' },
+		  { name: 'jquery', version: '^2.2.2' },
+		  { name: 'font-awesome', version: 'fontawesome#^4.5.0' }
+		];
+
+		const devDeps = [
+			{ name: 'moment', version: '^2.12.0' },
+		  { name: 'async', version: '^1.5.2' },
+		  { name: 'immutable', version: '^3.7.6' }
+		];
+
+		it('should extract dependencies', () => {
+			extractNodeOrBower(bowerJson, 'dependencies')
+				.should.deep.equal(deps);
+		});
+
+		it('should extract devDependencies', () => {
+			extractNodeOrBower(bowerJson, 'devDependencies')
+				.should.deep.equal(devDeps);
+		});
+
+		it('should detect bower.json properties', () => {
+			const detectBower = deedeeRewireAPI.__get__('detectBower');
+			detectBower(__dirname + '/fixture')
+				.should.deep.equal({
+					type: 'bower',
+					projectName: 'test',
+					dependencies: deps,
+					devDependencies: devDeps
 				});
 		});
 	});
